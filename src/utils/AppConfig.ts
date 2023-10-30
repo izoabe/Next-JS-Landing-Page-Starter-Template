ca44d0cce815e5e5f022a92c0b752a0ece892612
iggo <64318388+izoabe@users.noreply.github.com>
2023-10-30 15:50:18 +0700

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
+  site_name: 'izo',
+  title: 'izo',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
